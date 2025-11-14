c07c21fe82b99d0dd6a45e0fe22a930088f69d3b
prashantsah2 <prashant@clan.business>
2025-11-14 15:47:05 +0530

    fixed most reposve errors

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -19,7 +19,7 @@ export default function ContactPage() {
   return (
     <>
     <Header4 />
-    <div className="min-h-screen bg-[#fff] w-full max-w-[1371px] flex flex-col items-center justify-center p pt-20 pb-20 ">
+    <div className="min-h-screen bg-[#fff] w-full max-w-[1371px] flex flex-col items-center justify-center p pt-20 pb-20  max-sm:px-3 px-6">
      
      <QueryForm3/>
   
